4be881fd01961e419cb2916f7c28942aed26bfe2
Jin1c on imini <Jin0x31c@gmail.com>
2024-11-19 22:20:00 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,49 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, sleep, h } from "koishi";
+import {} from "koishi-plugin-adapter-onebot";
 
-export const name = 'group-notice'
+export const name = "group-notice";
 
-export interface Config {}
+export interface Config {
+  interval: number;
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  interval: Schema.number()
+    .role("slider")
+    .min(0.5)
+    .max(10)
+    .step(0.5)
+    .default(1)
+    .description("发送多组公告的间隔，单位秒"),
+});
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, { interval }: Config) {
+  ctx = ctx.platform("onebot").guild();
+  ctx.i18n.define("zh-CN", require("./locales/zh_CN"));
+  ctx
+    .command("group-notice [...groupIds:string]", "发群公告", { authority: 3 })
+    .alias("发公告")
+    .action(async ({ session }, ...groupIds) => {
+      if (groupIds.length == 0) {
+        groupIds.push(session.guildId);
+      }
+      session.send(session.text(".what-to-add"));
+      const reply = h.parse(await session.prompt());
+      let contents = [];
+      let alert_flag = false;
+      for (let e of reply) {
+        if (e.type === "text") {
+          contents.push(e);
+        } else {
+          alert_flag = true;
+        }
+      }
+      if (alert_flag) {
+        session.send(session.text(".type-warn"));
+      }
+      for (let groupId of groupIds) {
+        await session.onebot.sendGroupNotice(groupId, contents.join("\n"));
+        await sleep(interval * 1000);
+      }
+    });
 }
